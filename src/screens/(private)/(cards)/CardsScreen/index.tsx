d8669bbe602db9d89d0def59f5dc2d8d0f5e3c0e
Lucas Caldas <lucasmedeiroscaldas@yahoo.com.br>
2024-04-12 17:36:22 -0300

    add: tela e sistema de cartões

--- src/screens/(private)/(cards)/CardsScreen/index.tsx
+++ src/screens/(private)/(cards)/CardsScreen/index.tsx
@@ -1,5 +1,46 @@
+import { Box, ScrollView, View } from "@gluestack-ui/themed";
+import { useEffect, useState } from "react";
+import { useStateChange } from "../../../../contexts/stateChangeContext";
+import { fetchUserCards } from "../../../../service/api/cards";
+import { AddCardDialog } from "../../../../components/Dialogs/AddCardDialog";
+import { ChangeCardDialog } from "../../../../components/Dialogs/ChangeCardDialog";
+
 export default function CardsScreen() {
+    const [cards, setCards] = useState<Cartao[]>([]);
+    const { stateChanged } = useStateChange();
+
+    useEffect(() => {
+        handleUserCards();
+    }, [stateChanged]);
+
+    async function handleUserCards() {
+        try {
+            const data = await fetchUserCards();
+            setCards(data.content)
+            console.log(data.content)
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
-        <></>
+        <Box>
+            <ScrollView
+                style={{ height: '100%' }}
+                contentContainerStyle={{ flexGrow: 1 }}
+            >
+                <View backgroundColor={'$primary600'} h={48} mb={24}></View>
+                <View gap={16} padding={16} justifyContent="center" alignItems="center">
+                    <AddCardDialog />
+                    {cards.map((card) => (
+                        <ChangeCardDialog key={card.cartaoId} card={card} />
+                    ))}
+                </View>
+            </ScrollView>
+        </Box>
     );
-  };
+};
+
+
+
+
